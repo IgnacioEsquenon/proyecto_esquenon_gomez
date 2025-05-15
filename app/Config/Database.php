f12157634f1920474afb7b9e79e5a9bdb9185496
Ignacio Esquenón <esquenonn@gmail.com>
2025-05-15 09:30:27 -0300

    Commit 15/05

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
+        'username'     => 'root',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'bd_esquenon_gomez',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
